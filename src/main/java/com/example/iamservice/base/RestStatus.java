4243cf6352c66f739a87b8658664e575380767e6
lehonganh0201 <le2960620@gmail.com>
2026-06-09 09:59:12 +0700

    first commit

---
+++ src/main/java/com/example/iamservice/base/RestStatus.java
@@ -0,0 +1,12 @@
+package com.example.iamservice.base;/**
+ * ----------------------------------------------------------------------------
+ * Author:        Hong Anh
+ * Created on:    09/06/2026 at 9:55
+ * Project:       IAMService
+ * Contact:       https://github.com/lehonganh0201
+ * ----------------------------------------------------------------------------
+ */
+ 
+ 
+public enum RestStatus {
+}
