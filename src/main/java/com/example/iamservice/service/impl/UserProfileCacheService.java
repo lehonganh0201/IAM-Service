ae463e43e53a882d3477954b4c143061dd90bfa7
lehonganh0201 <le2960620@gmail.com>
2026-06-12 11:21:45 +0700

    convert cache from token to email

---
+++ src/main/java/com/example/iamservice/service/impl/UserProfileCacheService.java
@@ -0,0 +1,44 @@
+package com.example.iamservice.service.impl;
+
+import com.example.iamservice.domain.dto.response.UserResponse;
+import com.example.iamservice.domain.entity.User;
+import com.example.iamservice.exception.NotFoundException;
+import com.example.iamservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+/**
+ * ----------------------------------------------------------------------------
+ * Author:        Hong Anh
+ * Created on:    12/06/2026 at 11:08
+ * Project:       IAMService
+ * Contact:       https://github.com/lehonganh0201
+ * ----------------------------------------------------------------------------
+ */
+
+@Service
+@RequiredArgsConstructor
+public class UserProfileCacheService {
+
+    private final UserRepository userRepository;
+
+    @Cacheable(value = "users", key = "#email")
+    public UserResponse getUserProfileByEmail(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundException("User not found"));
+
+        return UserResponse.builder()
+                .fullName(user.getFullName())
+                .dateOfBirth(user.getDateOfBirth())
+                .phoneNumber(user.getPhoneNumber())
+                .email(user.getEmail())
+                .avatarUrl(user.getAvatarUrl())
+                .build();
+    }
+
+    @CacheEvict(value = "users", key = "#email")
+    public void evictUserProfile(String email) {
+    }
+}
